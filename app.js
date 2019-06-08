2255ceb7f99c7eee7719ebbe1aa55e9b51defa7f
chon-homo <hungmotlan@gmail.com>
2019-06-08 20:16:51 +0700

    da co update

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const mysql = require ('mysql');
 const express = require('express');
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const app = express();
 app.listen(3030);
 const statusDb= mysql.createConnection({
@@ -19,6 +21,15 @@ app.get('/getWarningValue',(req,res)=>{
     let sql = 'SELECT * FROM warning';
     statusDb.query(sql,(err , response)=>{
         if (err) throw err 
-        res.json(response);
+        res.json(response[0]);
+    })
+})
+app.post('/updateStatus',urlencodedParser,(req,res)=>{
+    console.log(req.body);
+    let sql ='UPDATE status SET status=' +req.body.status +' WHERE id=' +req.body.id;
+    console.log(sql);
+    statusDb.query(sql,(err,response)=>{
+        if (err) throw err 
+        res.json({status : "success"});
     })
 })
