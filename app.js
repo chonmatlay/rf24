de565f390a1137760445385b282230be09bf9125
chon-homo <hungmotlan@gmail.com>
2019-06-08 17:23:38 +0700

    m2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const mysql = require ('mysql');
 const express = require('express');
 const app = express();
-app.listen(3000);
+app.listen(3030);
 const statusDb= mysql.createConnection({
     host : "localhost",
     user : "root" ,
@@ -15,3 +15,10 @@ app.get('/getAllStatus',(req,res)=> {
         res.json(response)
     })
 });
+app.get('/getWarningValue',(req,res)=>{
+    let sql = 'SELECT * FROM warning';
+    statusDb.query(sql,(err , response)=>{
+        if (err) throw err 
+        res.json(response);
+    })
+})
